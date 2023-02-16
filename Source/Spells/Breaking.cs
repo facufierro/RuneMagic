1d961c70412f8c18afc9c9947fd8da240b293059
Facundo Fierro <fierrof_47@hotmail.com>
2023-02-16 12:58:44 +0100

    Implemented Breaking spell

--- Source/Spells/Breaking.cs
+++ Source/Spells/Breaking.cs
@@ -1,4 +1,9 @@
-﻿namespace RuneMagic.Source.Spells
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace RuneMagic.Source.Spells
 {
     public class Breaking : Spell
     {
@@ -12,12 +17,12 @@
         public override bool Cast()
         {
             //set Target to the debris under muse cursor
-            //Target = Game1.currentLocation.getObjectAtTile((int)Game1.currentCursorTile.X, (int)Game1.currentCursorTile.Y);
-            //if (Target is not null and Debris)
-            //{
-            //    return true;
-            //}
-            return false;
+            var pickaxe = new Pickaxe();
+            var target = Game1.currentLocation.getObjectAtTile((int)Game1.currentCursorTile.X, (int)Game1.currentCursorTile.Y);
+            if (target == null)
+                return false;
+            pickaxe.DoFunction(Game1.currentLocation, (int)target.TileLocation.X * Game1.tileSize, (int)target.TileLocation.Y * Game1.tileSize, 1, Game1.player);
+            return true;
         }
     }
 }
